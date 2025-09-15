e1ffa5f0984d4fae7a366da20ba03ab468a34555
Oleh Oprysk <opryskoleh2006@gmail.com>
2025-09-15 23:13:00 +0300

    fixed

--- src/task-6.ts
+++ src/task-6.ts
@@ -2,6 +2,10 @@ function getFirstElement<T>(arr: T[]): T {
   return arr[0];
 }
 
-getFirstElement([1, 2, 3]);           // 1
-getFirstElement(["a", "b", "c"]);     // "a"
-getFirstElement([true, false, true]); // true
+const firstNumber = getFirstElement<number>([1, 2, 3]);           // 1
+const firstString = getFirstElement<string>(["a", "b", "c"]);     // "a"
+const firstBoolean = getFirstElement<boolean>([true, false, true]); // true
+
+console.log(firstNumber);
+console.log(firstString);
+console.log(firstBoolean);
